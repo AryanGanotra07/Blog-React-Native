376dc1a9f3b320e62749815b9446bc02375b7b3b
Aryan Ganotra <aryanganotra7@gmail.com>
2019-10-06 20:50:01 +0530

    .

--- Blog/src/context/BlogContext.js
+++ Blog/src/context/BlogContext.js
@@ -37,9 +37,12 @@ const getBlogPosts = dispatch => {
 };
 
 const addBlogPost = (dispatch ) => {
-    return (title, content, callback)=> {
-    dispatch({type: 'add_blogpost',payload: {title, content}});
+    return async (title, content, callback)=> {
+        await jsonserver.post('/blogposts',{title, content});
+   // dispatch({type: 'add_blogpost',payload: {title, content}});
+   if(callback){
     callback();
+   }
     }
 };
 
--- Blog/src/screens/IndexScreen.js
+++ Blog/src/screens/IndexScreen.js
@@ -7,6 +7,13 @@ const IndexScreen =({navigation}) => {
     const {state, deleteBlogPost, getBlogPosts} = useContext(Context);
     useEffect(()=>{
         getBlogPosts();
+
+       const listener = navigation.addListener('didFocus', ()=> {
+            getBlogPosts();
+        });
+        return () => {
+            listener.remove();
+        };
     },[]);
     return (
         <View>
